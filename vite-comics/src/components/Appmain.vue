4935968e60845d6508dc8e7d1bb83004288cc60c
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-12-16 19:28:16 +0100

    categories completato

--- vite-comics/src/components/Appmain.vue
+++ vite-comics/src/components/Appmain.vue
@@ -10,27 +10,23 @@
                 <ul class="comics-categ">
                     <li class="comic-card">
                         <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>Digital comics</p>
+                        <p>DIGITAL COMICS</p>
                     </li>
                     <li class="comic-card">
-                        <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>DC Merchandise</p>
+                        <img src="../assets/vue-dc-comics-1/img/buy-comics-merchandise.png" class="comic-img" alt="merch">
+                        <p>DC MERCHANDISE</p>
                     </li>
                     <li class="comic-card">
-                        <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>Digital comics</p>
+                        <img src="../assets/vue-dc-comics-1/img/buy-comics-subscriptions.png" class="comic-img" alt="subs">
+                        <p>SUBSCRIPTION</p>
                     </li>
                     <li class="comic-card">
-                        <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>Digital comics</p>
+                        <img src="../assets/vue-dc-comics-1/img/buy-comics-shop-locator.png" class="comic-img" alt="shops location">
+                        <p>COMIC SHOP LOCATOR</p>
                     </li>
                     <li class="comic-card">
-                        <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>Digital comics</p>
-                    </li>
-                    <li class="comic-card">
-                        <img src="../assets/vue-dc-comics-1/img/buy-comics-digital-comics.png" class="comic-img" alt="digital comics">
-                        <p>Digital comics</p>
+                        <img src="../assets/vue-dc-comics-1/img/buy-dc-power-visa.svg" class="comic-img" alt="battery">
+                        <p>DC POWER VISA</p>
                     </li>
                 </ul>
             </div>
