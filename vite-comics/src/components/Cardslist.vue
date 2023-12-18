71675ac0625e291bebe2a69445b3745f25a79bed
“Bernardo <bernardobarbucci@gmail.com>
2023-12-18 17:53:32 +0100

    creato nuovo elemento cardslist.vue per creare le cards dei comics, da implementare adesso con v-for su app.vue

---
+++ vite-comics/src/components/Cardslist.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="comic-card">
+        <img :src="card.thumb" alt="Comic Thumbnail" />
+        <h3>{{ card.series }}</h3>
+        <p>{{ card.price }}</p>
+        <p>{{ card.type }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
